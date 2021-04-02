3bba17338a9f46a66110e3e2419b26aca334135f
unknown <suyashgaikwad31@gmail.com>
2021-04-02 20:30:18 +0530

    welcome to Invoice Service

---
+++ src/test/java/InvoiceServiceTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class InvoiceServiceTest {
+    @Test
+    public void givenDistanceAndTime_ShouldReturnTotalFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 2.0;
+        int time = 5;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assertions.assertEquals(25,fare,0.0);
+
+    }
+}
